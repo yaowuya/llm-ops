2334e92189c6ca8500aed604ef606398cd79e4ea
zhong <zhong@123.com>
2026-01-17 17:38:05 +0800

    feature: 新增upload_file model

--- internal/model/__init__.py
+++ internal/model/__init__.py
@@ -1,8 +1,5 @@
 from .api_tool import ApiTool, ApiToolProvider
 from .app import App
+from .upload_file import UploadFile
 
-__all__ = [
-    "App",
-    "ApiTool",
-    "ApiToolProvider",
-]
+__all__ = ["App", "ApiTool", "ApiToolProvider", "UploadFile"]
---
+++ internal/model/upload_file.py
@@ -0,0 +1,26 @@
+from sqlalchemy import UUID, Column, DateTime, Integer, PrimaryKeyConstraint, String, text
+
+from internal.extension.database_extension import db
+
+
+class UploadFile(db.Model):
+    """上传文件模型"""
+
+    __tablename__ = "upload_file"
+    __table_args__ = (PrimaryKeyConstraint("id", name="pk_upload_file_id"),)
+
+    id = Column(UUID, nullable=False, server_default=text("uuid_generate_v4()"))
+    account_id = Column(UUID, nullable=False)
+    name = Column(String(255), nullable=False, server_default=text("''::character varying"))
+    key = Column(String(255), nullable=False, server_default=text("''::character varying"))
+    size = Column(Integer, nullable=False, server_default=text("0"))
+    extension = Column(String(255), nullable=False, server_default=text("''::character varying"))
+    mime_type = Column(String(255), nullable=False, server_default=text("''::character varying"))
+    hash = Column(String(255), nullable=False, server_default=text("''::character varying"))
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        server_default=text("CURRENT_TIMESTAMP(0)"),
+        server_onupdate=text("CURRENT_TIMESTAMP(0)"),
+    )
+    created_at = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP(0)"))
